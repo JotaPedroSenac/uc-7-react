0f4136ffffe866ae7ec07c9fddfbc3bb3b8104e3
João Pedro <dev.jotapedro@gmail.com>
2025-03-12 13:16:06 -0300

    update: melhorado o cdd de band

--- atividade/atividade/src/components/Band/index.jsx
+++ atividade/atividade/src/components/Band/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import styles from "./Band.module.css"
+import styles from "./Band.module.css";
 
-function Band(){
-  return(
+function Band() {
+  return (
     <section className={styles.Band}>
-      <div className={styles.Band}>
-          <img src="https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg" alt="Banda 1"/>
-          <div class="band-info">
-              <h2>Banda 1</h2>
-              <p>Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
+      <div className={styles.bandContainer}> {/* Corrigido aqui */}
+        <img
+          src="https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg"
+          alt="Banda 1"
+        />
+        <div className={styles.bandInfo}>
+          <h2>Banda 1</h2>
+          <p>Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
       </div>
     </section>
   );
