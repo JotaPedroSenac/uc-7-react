2fcb016b5b75187f0d76fdd82aa32ab624df41c2
JOAO PEDRO DE SOUZA CORINGA <a95436285@rn.senac.local>
2025-03-12 17:27:40 -0300

    update: concluido página

--- atividade/atividade/src/components/Band/index.jsx
+++ atividade/atividade/src/components/Band/index.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import styles from "./Band.module.css";
 
-function Band() {
+function Band({imagem, nome, descricao}) {
   return (
-    <section className={styles.Band}>
-      <div className={styles.bandContainer}> {/* Corrigido aqui */}
+    <div className={styles.band}>
         <img
-          src="https://cdn.ontourmedia.io/gunsnroses/site_v2/animations/gnr_loop_logo_01.jpg"
-          alt="Banda 1"
+          src={imagem}
+          alt={nome}
         />
         <div className={styles.bandInfo}>
-          <h2>Banda 1</h2>
-          <p>Descrição da Banda 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h2>{nome}</h2>
+          <p>{descricao}</p>
         </div>
-      </div>
-    </section>
+    </div>
   );
 }
 
