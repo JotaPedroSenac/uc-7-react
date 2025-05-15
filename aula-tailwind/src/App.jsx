3d67de283929e6ccbce7f8efa81347876e549ca2
JOAO PEDRO DE SOUZA CORINGA <a95436285@rn.senac.local>
2025-05-15 16:37:24 -0300

    aula tailwind

---
+++ aula-tailwind/src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+
+  return (
+    <>
+           {/* <div
+        className="mx-auto flex max-w-sm items-center p-6 shadow-lg bg-red-50">
+        <div className="text-xl font-medium text-black dark:text-white">
+          <h1>Hello World, React + Tailwind</h1>
+          <p className="text-gray-500 dark:text-gray-400">Tentaremos formatar algo!</p>
+        </div>
+      </div> */}
+      <div className="min-h-screen bg-blue-100 flex items-center justify-center ">
+        <div className="p-8 bg-whithe roundend-2x1 shadow-lg">
+          <h1 className="text-2x1 font-bold text-purple-600">Hello World, React + Tailwind</h1>
+          <p className="mt-2 text-gray-600">Tentaremos formatar algo!</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
